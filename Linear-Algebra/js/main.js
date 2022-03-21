8486799da29af2995a326aa3422aa79b98d0db9f
Miguel Angelo <masbicudo@gmail.com>
2022-03-20 23:06:57 -0300

    Orbit control from jsdeliver

--- Linear-Algebra/js/main.js
+++ Linear-Algebra/js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'https://cdn.skypack.dev/three'
 import { TrackballControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/TrackballControls'
-import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls'
+// import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three-orbitcontrols@2.110.3/OrbitControls.js'
 
 import { Arrow } from './arrow.js'
 import { combine } from './func.js'
